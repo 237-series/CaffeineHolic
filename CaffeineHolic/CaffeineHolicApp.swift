e23dd0ac8cacb6499f12724d48dcf2380a12cfde
sglee237 <sglee237@gmail.com>
2023-01-15 15:06:34 +0900

    Initial Commit

---
+++ CaffeineHolic/CaffeineHolicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeineHolicApp.swift
+//  CaffeineHolic
+//
+//  Created by sglee237 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct CaffeineHolicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
