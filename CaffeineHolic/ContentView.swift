fb77c0a0063a5bda93a61091afe52ee6b4eff03c
sglee237 <sglee237@gmail.com>
2023-01-16 17:28:47 +0900

    1. Setting View Stack with Simple Data

--- CaffeineHolic/ContentView.swift
+++ CaffeineHolic/ContentView.swift
@@ -7,15 +7,62 @@
 
 import SwiftUI
 
+
+struct Daily: Identifiable {
+    let id = UUID()
+    let title:String
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var dailyList = ["First", "Second"]
+    
+    func incList() {
+        dailyList.insert("New", at: 0)
+    }
+    
+    var DailyView: some View {
+        // Daily History area
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 20)  {
+                List(dailyList, id: \.self) {item in
+                    Text(item)
+                }
+                Spacer()
+                Text("") // End of Daily Item List
+            }
+            .frame(width: 200)
+            .opacity(0.3)
+            Spacer()
         }
         .padding()
+    }
+    
+    var Buttons: some View {
+        VStack {
+            // Top buttons
+            HStack(spacing: 20){
+                Text("")
+                Spacer()
+                Text("Top Button")
+            }
+            Spacer()
+            Button("Go to Caffeine") {
+                print("Click")
+                incList()
+            }
+            Spacer()
+            Text("Bottom")
+        }
+        .padding()
+        
+    }
+    
+    
+    var body: some View {
+        ZStack {
+            DailyView
+            Buttons
+        }
     }
 }
 
