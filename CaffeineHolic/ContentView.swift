56203922f02d44656b2a4218ff2689ce27a336ee
sglee237 <sglee237@gmail.com>
2023-01-16 18:06:14 +0900

    Adding Profile Image and Add ImageView

--- CaffeineHolic/ContentView.swift
+++ CaffeineHolic/ContentView.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
                 }
             }
             Spacer()
+            Image("Profile.Intro")
             Button("Get Stress") {
                 print("")
                 incStress()
