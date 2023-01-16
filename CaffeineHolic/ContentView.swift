b13e446d165d50e244cbccafd851f37a9aa5be0e
sglee237 <sglee237@gmail.com>
2023-01-16 17:47:24 +0900

    Adding 3 Buttons & Actions

--- CaffeineHolic/ContentView.swift
+++ CaffeineHolic/ContentView.swift
@@ -20,6 +20,18 @@ struct ContentView: View {
         dailyList.insert("New", at: 0)
     }
     
+    func incStress() {
+        dailyList.insert("Stress Up", at: 0)
+    }
+    
+    func incCoffee() {
+        dailyList.insert("Get Some Coffee", at: 0)
+    }
+    
+    func getRest() {
+        dailyList.insert("REST", at: 0)
+    }
+    
     var DailyView: some View {
         // Daily History area
         HStack(alignment: .top) {
@@ -43,14 +55,28 @@ struct ContentView: View {
             HStack(spacing: 20){
                 Text("")
                 Spacer()
-                Text("Top Button")
+//                Text("Top Button")
+                Button("Top Button") {
+                    print("Top Button Click")
+                    
+                }
             }
             Spacer()
+            Button("Get Stress") {
+                print("")
+                incStress()
+            }
+            .padding()
+            
             Button("Go to Caffeine") {
                 print("Click")
-                incList()
+//                incList()
+                incCoffee()
             }
+            .padding()
+            
             Spacer()
+            
             Text("Bottom")
         }
         .padding()
